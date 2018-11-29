d73b08d6ac1511a81aec501df5bb8d0e5f653b76
oscarfrykedal <oscar_frykedal@hotmail.com>
2018-11-29 16:19:54 +0100

    send a message

--- Hej/app/src/main/java/com/example/oscar/hej/Adapter/UserAdapter.java
+++ Hej/app/src/main/java/com/example/oscar/hej/Adapter/UserAdapter.java
@@ -1,6 +1,7 @@
 package com.example.oscar.hej.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -10,6 +11,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.example.oscar.hej.MessageActivity;
 import com.example.oscar.hej.R;
 import com.example.oscar.hej.User;
 
@@ -36,9 +38,8 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
-        //kan va fel!
 
-        User user = mUser.get(position);
+        final User user = mUser.get(position);
         holder.username.setText(user.getUsername());
         if (user.getImageURL().equals("default")){
             holder.profile_image.setImageResource(R.mipmap.ic_launcher);
@@ -46,6 +47,14 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
             Glide.with(mContext).load(user.getImageURL()).into(holder.profile_image);
         }
 
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, MessageActivity.class);
+                intent.putExtra("userid", user.getId());
+                mContext.startActivity(intent);
+            }
+        });
     }
 
     @Override
--- Hej/app/src/main/java/com/example/oscar/hej/MessageActivity.java
+++ Hej/app/src/main/java/com/example/oscar/hej/MessageActivity.java
@@ -1,13 +1,115 @@
 package com.example.oscar.hej;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CircleCrop;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MessageActivity extends AppCompatActivity {
+
+    CircleImageView profile_image;
+    TextView username;
+
+    FirebaseUser fuser;
+    DatabaseReference reference;
+
+    ImageButton btn_send;
+    EditText text_send;
+
+
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        profile_image = findViewById(R.id.profile_image);
+        username = findViewById(R.id.username);
+        btn_send= findViewById(R.id.btn_send);
+        text_send = findViewById(R.id.text_send);
+
+        intent = getIntent();
+        final String userid =intent.getStringExtra("userid");
+        fuser = FirebaseAuth.getInstance().getCurrentUser();
+
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = text_send.getText().toString();
+                if (!msg.equals("")){
+                    sendMessage(fuser.getUid(), userid, msg);
+                }else{
+                    Toast.makeText(MessageActivity.this, "you need to write something", Toast.LENGTH_SHORT).show();
+                }
+                text_send.setText("");
+            }
+        });
+
+
+        reference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                username.setText(user.getUsername());
+                if (user.getImageURL().equals("default")){
+                    profile_image.setImageResource(R.mipmap.ic_launcher);
+                }else{
+                    Glide.with(MessageActivity.this).load(user.getImageURL()).into(profile_image);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+    }
+
+    private void sendMessage(String sender, String receiver, String message) {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("sender", sender);
+        hashMap.put("reciever", receiver);
+        hashMap.put("message", message);
+
+        reference.child("Chats").push().setValue(hashMap);
     }
 }
